aea9bed5310637051fc5d26dafd8b802dea96e7c
Michael <mnichau@yahoo.de>
2015-08-25 00:21:39 +0100

    New Yeoman AngularJS app for GitHub Pages

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vocabStashApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the vocabStashApp
+ */
+angular.module('vocabStashApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vocabStashApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vocabStashApp
+ */
+angular.module('vocabStashApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
